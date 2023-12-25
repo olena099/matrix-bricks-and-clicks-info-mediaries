f9802f9a2c553717ebf58088fe00ff2de940d643
olena099 <142909583+olena099@users.noreply.github.com>
2023-12-25 14:06:13 -0800

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,30 +1,65 @@
+    public static void main(String[] args) {
+        }
 
-    const data = [];
-    const data = generateRandomData();
-    });
-    for (let i = 0; i < 10; i++) {
 
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
-        console.log("Random Number: " + item);
-
-function main() {
-main();
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
     }
-    return data;
-function generateRandomData() {
-}    main()
-    return data
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+}
+            System.out.println("Random Number: " + item);
+        for (int item : data) {    return data
+    main()
     data = generate_random_data()
-        print(f"Random Number: {item}")
-def main():
-import random
 
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+def main():
 
     for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
 if __name__ == "__main__":
-
+        print(f"Random Number: {item}")
+    for item in data:
 
     data = [random.randint(1, 100) for _ in range(10)]
 def generate_random_data():
+
+    return data
+
+import random
+    main()
+def main():
+    data = generate_random_data()
+
+if __name__ == "__main__":import random
+    main()
+    for item in data:
+
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+def main():
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+    data = generate_random_data()
+    main()
+def main():
+import random
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+
+if __name__ == "__main__":
+    return data
